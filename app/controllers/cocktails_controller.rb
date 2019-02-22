83a2e25f044667737e32b5cb8c4c501590fd3bc7
Phuong To <tophu95@gmail.com>
2019-02-22 13:02:34 +0000

    Completed Controller Actions and Controller Routes

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,10 +1,11 @@
 class CocktailsController < ApplicationController
-  before_action :set_cocktail, only: [:show]
+  # before_action :set_cocktail, only: [:show]
   def index
     @cocktails = Cocktail.all
   end
 
   def show
+    @cocktail = Cocktail.find(params[:id])
   end
 
   def new
@@ -22,9 +23,9 @@ class CocktailsController < ApplicationController
 
   private
 
-  def set_cocktail
-    @cocktail = Cocktail.find(params[:id])
-  end
+  # def set_cocktail
+  #   @cocktail = Cocktail.find(params[:id])
+  # end
 
   def cocktail_params
     params.require(:cocktail).permit(:name)
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,16 +1,16 @@
 class DosesController < ApplicationController
   before_action :set_dose, only: [:destroy]
   def new
-    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
   end
 
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
+
     if @dose.save
-      @dose.save
       redirect_to cocktail_path(@cocktail)
     else
       render :new
@@ -29,6 +29,6 @@ class DosesController < ApplicationController
   end
 
   def dose_params
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
