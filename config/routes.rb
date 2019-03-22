eb0c34e69cfc7239689fc30d8a49bd326436425e
Phuong To <tophu95@gmail.com>
2019-03-22 15:14:17 +0000

    added redirect link on logo inside the nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root routes --
-  root 'cocktails#index'
+  root to: 'cocktails#index'
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:new, :create]
     resources :reviews, only: [:create]
