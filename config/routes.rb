5853589563a1f889c7dbff7edbd03c620efb839b
Phuong To <tophu95@gmail.com>
2019-03-24 19:05:09 +0000

    can access all routes of cocktail except delete, can edit cocktail, which redirects user to the edit cocktail page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root routes --
   root to: 'cocktails#index'
-  resources :cocktails, only: [:index, :show, :new, :create, :update] do
+  resources :cocktails, except: [:destroy] do
     resources :doses, only: [:new, :create]
     resources :reviews, only: [:create]
   end
